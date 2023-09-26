6fabf2d0f617bce4398e0360a2b165bc8528ceed
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-09-26 11:23:31 +0530

    set application run in port 8001

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Node server is running on port ${port}`);
+});
