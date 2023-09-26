47ba0c5880cd79b3f9e548c8ff268c8d60c3de34
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-09-26 12:15:22 +0530

    connect mongodb database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Connection Error: ", err));
 
 const port = process.env.PORT;
 
