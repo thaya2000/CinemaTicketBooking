a738b6f83f3108b3c3cd33332b8dc6aa56fdd93d
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-10-10 06:43:20 +0530

    CRUD for category

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import morgan from "morgan";
-import authRoutes from './routes/auth.js';
+import authRoutes from "./routes/auth.js";
+import categoryRoutes from "./routes/category.js";
 
 dotenv.config();
 
@@ -13,14 +14,13 @@ mongoose
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log("DB Connection Error: ", err));
 
-
 // middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 
 // router middlewares
 app.use("/api", authRoutes);
-
+app.use("/api", categoryRoutes);
 
 const port = process.env.PORT;
 
