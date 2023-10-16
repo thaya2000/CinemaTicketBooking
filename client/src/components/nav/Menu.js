7a2ca67eb5db4834471548f62949d53dd38b75f7
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-10-16 16:05:14 +0530

    add logout user

--- client/src/components/nav/Menu.js
+++ client/src/components/nav/Menu.js
@@ -1,6 +1,16 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../context/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function Menu() {
+  const [auth, setAuth] = useAuth();
+  const navigate = useNavigate();
+  const logout = () => {
+    setAuth({ ...auth, user: null, token: "" });
+    localStorage.removeItem("auth");
+    navigate("/login");
+  };
+
   return (
     <>
       {/* <ul className="nav d-flex justify-content-around shadow-sm mb-2">
@@ -53,6 +63,11 @@ export default function Menu() {
                 <NavLink className="nav-link" to="/register">
                   Register
                 </NavLink>
+              </li>
+              <li className="nav-item pointer">
+                <a onClick={logout} className="nav-link">
+                  Logout
+                </a>
               </li>
             </ul>
             <form class="d-flex" role="search">
